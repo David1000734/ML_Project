5c048298fa6514ee31baff4d708a457760f9e413
David <lam.david459@yahoo.com>
2024-04-30 23:31:47 -0700

    Formatting
    Final edits

--- LinReg.py
+++ LinReg.py
@@ -2,9 +2,7 @@ import pandas as pd
 import numpy
 from math import sqrt
 
-
 def main():
-
     # load in data and start pre-procesing it
     BaseData = pd.read_csv('diabetes_binary_5050split_health_indicators_.csv', header=None)    # import data into dataframe
     BaseData = BaseData.iloc[1:]  # strips headers
@@ -13,10 +11,9 @@ def main():
     BaseData = BaseData.reset_index(drop=True)              # reset indices
     BaseData = BaseData.drop(BaseData.index[:92]).reset_index(drop=True)  # drop first sample and reset indices
 
-    # print(len(BaseData))
-    # print(BaseData)
     print('\t\t\tRMSE')
 
+    numList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21]
 
     # CV - manual implementation
     numFolds = 100
@@ -36,41 +33,30 @@ def main():
             train[j] = (train[j] - xmean) / xstd
             test[j] = (test[j] - xmean) / xstd
 
-
         # Regression Calculations
         VectorX = train.drop(columns=0) #drop diabetes column from X
         VectorX = pd.DataFrame(numpy.c_[numpy.ones(len(VectorX)), VectorX])     # add ones column to X
-        VectorY = train.drop(columns=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21])   #drop everything but diab from Y
+        VectorY = train.drop(columns = numList)   #drop everything but diab from Y
 
         #Perform the same data processing on test data sets
         testX = test.drop(columns=0)
         testX = pd.DataFrame(numpy.c_[numpy.ones(len(testX)), testX])
-        testY = test.drop(columns=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21])
-        # print(VectorY)
-        # print(VectorX)
+        testY = test.drop(columns = numList)
 
         #calculate b vector with the formatted training data
         b = numpy.dot(VectorX.transpose(), VectorX)
         b = numpy.linalg.inv(b)
         b = numpy.dot(b, VectorX.transpose())
         b = numpy.dot(b, VectorY)
-        #print(b)
 
         #Calculate Y vector using test data and coefficient s
         CalcY = numpy.dot(testX, b)
         PredictY = pd.DataFrame(CalcY)
-        #print(PredictY)
-        #print(VectorY)
-
 
         #Calculate RMSE
         difsq = (PredictY - testY)**2
         sums = difsq.sum()
         RMSE = sqrt(sums[0]/fSize)
-
-        # print(PredictY)
-        # print(PredictY[0][0])
-        # print(PredictY[0][1])
 
         # convert regression values into actual values
         for num in range(0, len(PredictY)):
@@ -79,17 +65,12 @@ def main():
             else:
                 PredictY.loc[num, 0] = 0
 
-        #print(PredictY)
-
         tSum = 0
         for num in range(0, len(PredictY)):
             if PredictY[0][num] == testY[0][num]:
                 tSum += 1
 
-        # print(tSum)
         acc = "Accuracy is: " + str(tSum / len(PredictY)) + '\n'
-        #print(acc)
-
 
         #print table results for current fold
         if (i+1) % 10 == 0:
@@ -100,6 +81,5 @@ def main():
             output += "{:.4f}".format(RMSE)
             print(output)
             print(acc)
-
 #run main
 main()
